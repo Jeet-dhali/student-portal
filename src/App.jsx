b39572c0c4e4226d22ac53fd0e88928b6ae622b6
jeet dhali <jeetproxy@gmail.com>
2025-09-23 13:27:15 +0530

    finished header and welcome text

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Navbar} from './components/header.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='header'>
+        <Navbar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='content'>
+        <h3 className='welcome-text'>Welcome to</h3>
+        <h1 className='main-title'>NIT Nagaland</h1>
+        <h2 className='subtitle'>Student Council</h2>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/main-body.jsx
@@ -0,0 +1,15 @@
+import '../components/main-body.css'
+
+export function MainBody() {
+
+    return (
+        <>
+            <div className='main'>
+                <h2 className="welcome-text">Welcome To</h2>
+                <h1 className="main-title">NIT Nagaland</h1>
+                <h3 className="subtitle">Student Club</h3>
+            </div>
+        </>
+    )
+
+}
