94418146c366dc61f75ad0d7f76d1aa379ef1300
jeet dhali <jeetproxy@gmail.com>
2025-09-30 19:32:39 +0530

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ function App() {
         
         <Routes>
           <Route index element={<HomePage />} />
-          <Route path="signup" element={<SignUpPage />} />
-          <Route path="login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
         
       
